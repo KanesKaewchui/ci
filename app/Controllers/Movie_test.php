27da0fe171da19c4585b3bae1e47e63b6b6411b4
Office <kaneskaewchui1@gmail.com>
2024-06-28 12:06:14 +0700

    5

--- app/Controllers/Movie_test.php
+++ app/Controllers/Movie_test.php
@@ -22,4 +22,55 @@ class Movie_test extends Controller
     {
         return view('viewsmovie');
     }
+
+    public function register()
+    {
+        if ($this->request->getMethod() == 'post') {
+            $user_name = $this->request->getPost('username');
+            $password = $this->request->getPost('password');
+            $name = $this->request->getPost('name');
+            $lastname = $this->request->getPost('lastname');
+            $email = $this->request->getPost('email');
+
+            $data = [
+                'user_name' => $user_name,
+                'password' => $password,
+                'name' => $name,
+                'lastname' => $lastname,
+                'email' => $email,
+                'status' => 1,
+                'create_time' => date('Y-m-d H:i:s')
+            ];
+
+            $query = $this->mydev_model->insert($data);
+
+            if ($query) {
+                echo "Registration successful<br>";
+                echo "Username: " . $user_name . "<br>";
+                echo "Password: " . $password . "<br>";
+                echo "Name-LastName: " . $name . " " . $lastname . "<br>";
+                echo "Email: " . $email . "<br>";
+                echo "Created Time: " . $data['create_time'];
+            } else {
+                echo "Error: " . $this->mydev_model->db_group_name->error();
+            }
+        } else {
+            return view('viewsmovie');
+        }
+    }
+
+    // public function login()
+    // {
+    //     if ($this->request->getMethod() == 'post') {
+    //         $username = $this->request->getPost('username');
+    //         $password = $this->request->getPost('password');
+
+    //         $sql = "SELECT * FROM user WHERE user_name = '$username' AND password  = '$password'";
+    //         $query = $this->mydev_model->select($sql);
+
+    //         if (isset($query)) {
+    //             echo "Login successful" . "<br>";
+    //         }
+    //     }
+    // }
 }
--- app/Views/viewsmovie.php
+++ app/Views/viewsmovie.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-    <form id="myForm" action="process_views" method="POST">
+    <form id="myForm" action="Movie_test" method="POST">
         <style>
             table,
             th,
@@ -26,6 +26,7 @@
                 margin-top: 10px;
             }
         </style>
+        <!-- Registration -->
         <table style="width:350px; margin:0 auto">
             <tr>
                 <th>Label</th>
@@ -40,34 +41,43 @@
                 <td><input type="password" name="password" id="password" required></td>
             </tr>
             <tr>
-                <td style="text-align:right">Confirm Password:</td>
-                <td><input type="password" name="confirmpassword" id="confirmpassword" required></td>
+                <td style="text-align:right">name:</td>
+                <td><input type="text" name="name" id="name" required></td>
             </tr>
             <tr>
-                <td style="text-align:right">Name:</td>
-                <td> <input type="text" name="name" id="name" required></td>
-            </tr>
-            <tr>
-                <td style="text-align:right">Last Name:</td>
+                <td style="text-align:right">lastname:</td>
                 <td><input type="text" name="lastname" id="lastname" required></td>
             </tr>
             <tr>
-                <td style="text-align:right">Number: </td>
-                <td><input type="number" name="number" id="number" required></td>
-            </tr>
-            <tr>
-                <td style="text-align:right">Email:</td>
+                <td style="text-align:right">email:</td>
                 <td><input type="email" name="email" id="email" required></td>
-            </tr>
-            <tr>
-                <td style="text-align:right">Account:</td>
-                <td><input type="number" name="account" id="account" required></td>
             </tr>
             <tr>
                 <td></td>
                 <td><input type="submit" name="submit" id="submit"></td>
             </tr>
         </table>
+
+        <br>
+
+        <!-- login -->
+        <!-- <table style="width:350px; margin:0 auto">
+            <tr>
+                <th>Label</th>
+                <th>data</th>
+            </tr>
+            <tr>
+                <td style="text-align:right">Username:</td>
+                <td><input type="text" name="username" id="username" required></td>
+            </tr>
+            <tr>
+                <td style="text-align:right">Password:</td>
+                <td><input type="password" name="password" id="password" required></td>
+            </tr>
+            <td></td>
+            <td><input type="submit" name="submit" id="submit"></td>
+            </tr>
+        </table> -->
     </form>
     <div id="error"></div>
 </body>
